6100cdbb8c2252e65f766b561ff1fe1bafaa5663
imoncoc <imoncoc0@gmail.com>
2024-02-05 18:25:19 +0600

    -

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-console.log("Hello jss");
+
+
+const accomondation = {
+    "accommodation_id": "",
+    "room_category_id": "",
+    "room_type_id": "",
+    "per_night_room_price": "",
+    "suitable_for_persons": "",
+    "is_shareable": true,
+    "number_of_beds": "",
+    "bed_configuration": ""
+}
+
+const accomondation2 = {
+    "accomondation": {
+        "accommodation_id": "",
+        "room_category_id": "",
+        "room_type_id": {
+            "label": "Couple",
+            "value": 1
+        },
+        "per_night_room_price": "",
+        "suitable_for_persons": "",
+        "is_shareable": true,
+        "number_of_beds": "",
+        "bed_configuration": ""
+    }
+}
+
+
+const accomondation3 = {
+    "accommodation_id": null,
+    "room_category_id": "",
+    "room_type_id": "",
+    "per_night_room_price": "",
+    "suitable_for_persons": "",
+    "is_shareable": true,
+    "number_of_beds": "",
+    "bed_configuration": ""
+}
+
+console.log(accomondation)
+console.log(accomondation2)
+console.log(accomondation3)
+
+console.log((accomondation?.room_type_id?.label === undefined || accomondation?.room_type_id?.label?.length === 0))
+
